938f4fea78b907b496a415a8d66ed3a5eae8783d
Brixter Porras <brixterporras@gmail.com>
2023-02-07 15:26:49 +0800

    feat: added message layout

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -14,7 +14,7 @@ import Link from "@mui/material/Link";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { Box } from "@mui/system";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { socket } from "../app/services/api";
 import { useLogoutMutation } from "../app/services/authServices";
 import { useGetUserQuery } from "../app/services/userServices";
@@ -26,221 +26,220 @@ interface Message {
   user: boolean;
 }
 
-const messages: Message[] = [
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    user: false,
-  },
-  {
-    message:
-      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message:
-      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, , comes from a line in section 1.10.32.",
-    user: false,
-  },
-  {
-    message:
-      "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
-    user: false,
-  },
-  {
-    message:
-      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-  {
-    message: "hi",
-    user: true,
-  },
-  {
-    message: "hello",
-    user: false,
-  },
-];
-
 export default function Dashboard() {
   const [logoutMutation, { isLoading: isLogoutLoading }] = useLogoutMutation();
   const { data, isLoading: isUserLoading } = useGetUserQuery("");
+  const [messages, setMessages] = useState<Message[]>([
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      user: false,
+    },
+    {
+      message:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message:
+        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, , comes from a line in section 1.10.32.",
+      user: false,
+    },
+    {
+      message:
+        "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
+      user: false,
+    },
+    {
+      message:
+        "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+    {
+      message: "hi",
+      user: true,
+    },
+    {
+      message: "hello",
+      user: false,
+    },
+  ]);
 
   const messagesEndRef = useRef<HTMLDivElement>(null);
 
@@ -261,10 +260,14 @@ export default function Dashboard() {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    messages.push({
-      message: data.get("message") as string,
-      user: true,
-    });
+    setMessages((prev) => [
+      ...prev,
+      {
+        message: data.get("message") as string,
+        user: true,
+      },
+    ]);
+    event.currentTarget.reset();
   };
 
   return (
